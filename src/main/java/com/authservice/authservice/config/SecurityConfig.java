60603dfa1b333ac09128062f0b813f2821bcfb00
Mateusz <mateusz.adamowicz.lo1@gmail.com>
2025-09-07 20:07:26 +0200

    Adding user service to cors

--- src/main/java/com/authservice/authservice/config/SecurityConfig.java
+++ src/main/java/com/authservice/authservice/config/SecurityConfig.java
@@ -40,6 +40,7 @@ public CorsConfigurationSource corsConfigurationSource() {
     CorsConfiguration configuration = new CorsConfiguration();
     configuration.setAllowedOrigins(List.of(
         "https://academic-stress-detector-client-ser.vercel.app",
+        "https://academic-stress-detector-user-service.onrender.com",
         "http://localhost:5173"
     ));
     configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
